ef1fadc92554a9f63b2990248ee0fbf1148f3a7b
Abdullah Faruque <90860472+afaruque30@users.noreply.github.com>
2021-09-30 20:38:37 -0400

    Update and rename greet.java to Greet.java

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,14 @@
+public class Greet {
+	public static void greet(String input){
+		System.out.printf("Why, hello there, %s. How do you do?", input);
+	public static void main(String[] args) {
+		number=*=(int)(math.random());
+		if (number == 0){
+			greet("Russel");
+		}else if(number==1){
+			greet("Kevin");
+		}else{
+			greet("joe");
+		}
+	}
+}
