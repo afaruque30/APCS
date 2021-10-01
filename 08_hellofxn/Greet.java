0c3299bca6e1ddfeb6dd159de32deb5669667c72
Abdullah Faruque <90860472+afaruque30@users.noreply.github.com>
2021-09-30 21:17:55 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,3 +1,17 @@
+/*
+Abdullah Faruque
+APCS
+HW08: Refactor Freshie Zero
+2021-09-30
+*/
+
+/**
+DISCOVERIES
+Main function can call helper function. In this case, the main function calls greet.
+UNRESOLVED QUESTIONS
+Is there a way to make it randomly call out a name, rather than all three at once. 
+*/
+
 public class Greet {
 	public static void greet(String input){
 		System.out.printf("Why, hello there, %s. How do you do? \n", input);
@@ -8,7 +22,4 @@ public class Greet {
 		greet("Russel");
 	}
 }
-/* 
-Discoveries-Main function can call helper function. In this case, the main function calls greet.
-Unresolved Questions- is there any other way to do this, and am I even doing it right.
-*/
+
