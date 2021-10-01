e949b112c7e3d3c4f0b45bd711f8482de564a8ca
Abdullah Faruque <90860472+afaruque30@users.noreply.github.com>
2021-09-30 20:54:57 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -3,7 +3,7 @@ public class Greet {
 		System.out.printf("Why, hello there, %s. How do you do?", input);
 	}
 	public static void main(String[] args) {
-		number=(int)(math.random());
+		number=(int)(Math.random());
 		if (number == 0){
 			greet("Russel");
 		}else if(number==1){
