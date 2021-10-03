1cb8634601ac50ac2e4d0c3c47657ccb1d126e40
Abduller Maraco <afaruque@stuy.edu>
2021-10-03 15:35:47 -0400

    no class files here

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,11 @@
+/*
+ABdullah Faruque + Green Lantern
+APCS
+HW09: What Are BigSibs Good For?
+2021-10-01
+*/
+public class BigSib {
+	public static void greet(String input){
+		System.out.printf("Why, hello there, %s. How do you do? \n", input);
+	}
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,19 @@
+/*
+ABdullah Faruque + Green Lantern
+APCS
+HW09: What Are BigSibs Good For?
+2021-10-01
+*/
+public class Greet {
+	public static void main(String[] args){
+		BigSib.greet("Russel");
+		BigSib.greet("Tasnim");
+		BigSib.greet("Abdullah");
+	}
+}
+/**
+DISCOVERIES
+none at the moment
+UNRESOLVED QUESTIONS
+none at the moment
+*/
