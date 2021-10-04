c144b8b0c130650214b5612ed05b2970fb44c70b
Abduller Maraco <afaruque@stuy.edu>
2021-10-04 18:59:24 -0400

    added hw 10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,11 @@
+/*
+ABdullah Faruque + Green Lantern
+APCS
+HW09: What Are BigSibs Good For?
+2021-10-01
+*/
+public class BigSib {
+	public static String greet(String input){
+		return "Why, hello there, " + input + ". How do you do?";
+	}
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,19 @@
+/*
+ABdullah Faruque + Green Lantern
+APCS
+HW10: Refactor Big Sib One
+2021-10-04
+*/
+public class Greet {
+	public static void main(String[] args){
+		System.out.println(BigSib.greet("Russel") );
+        System.out.println(BigSib.greet("Tasnim") );
+        System.out.println(BigSib.greet("Abdullah") );
+    }
+}
+/**
+DISCOVERIES
+You can print out the output of the function
+UNRESOLVED QUESTIONS
+which way is more efficient, hw9s method or hw10?
+*/
