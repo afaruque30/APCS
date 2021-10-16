e963695a6231310ad9068a71e6c146dad4a16f07
Abdullah Faruque <90860472+afaruque30@users.noreply.github.com>
2021-10-16 19:23:29 -0400

    Update BankAccount.java

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -6,7 +6,8 @@ public class BankAccount {
 	private int userID;
 	private int bal;
 	public BankAccount(String pname, String ppassword, int pPIN, int pbal){
-		userID = (BankAccount.lastID + 1) % 10^9;
+		userID = BankAccount.lastID;
+		userID=(userID+ 1) % 10^9;
 		BankAccount.lastID += 1;
 		name = pname;
 		password = ppassword;
@@ -15,15 +16,86 @@ public class BankAccount {
 	}
 	public void printInfo(int pPIN){
 		if (PIN == pPIN) {
-			System.out.println("ACCOUNT SUMMARY\n----------------------\nNAME: " + name + "\nUSER ID: " + Integer.toString(userID) + "\nBALANCE: $" + Integer.toString(bal));
+			System.out.println("ACCOUNT SUMMARY\n----------------------\nNAME: " + name + "\nUSER ID: " + userID + "\nBALANCE: $" + bal + "\nPASSWORD: " + password + "\nPIN: " + PIN);
 		}
 		else {
 			System.out.println("PIN was incorrect");
 		}
 	}
+	public void setBal(int pPIN, int newBal){
+		if (PIN==pPIN){
+			bal=newBal;
+		}
+		else{
+			System.out.println("PIN was incorrect");
+		}
+	}
+	public void setName(int pPIN, String newName){
+		if (PIN==pPIN){
+			name=newName;
+		}
+		else{
+			System.out.println("PIN was incorrect");
+		}
+	}
+	public void setPassword(int pPIN, String newPass){
+		if (PIN==pPIN){
+			password=newPass;
+		}
+		else{
+			System.out.println("PIN was incorrect");
+		}
+	}
+	public void setPIN(int pPIN, int newPIN){
+		if (PIN==pPIN){
+			PIN=newPIN;
+		}
+		else{
+			System.out.println("PIN was incorrect");
+		}
+	}
+	public void depositMoney(int pPIN, int money){
+		if (PIN==pPIN){
+			if (money>0){
+				bal+=money;
+			}else {
+				System.out.println("Cannot deposit negative amount, try again");
+			}
+		}
+		else{
+			System.out.println("PIN was incorrect");
+		}
+	}
+	public void withdrawMoney(int pPIN, int money){
+		if (PIN==pPIN){
+			if (money>0){
+				bal-=money;
+			}else {
+				System.out.println("Input must be positive");
+			}
+		}
+		else{
+			System.out.println("PIN was incorrect");
+		}
+	}
+	
+	
 	public static void main(String[] args){
 		BankAccount john = new BankAccount("John Doe", "s3cr3tpassw0rd", 1234, 100389091);
 		john.printInfo(5432);
 		john.printInfo(1234);
+		john.setName(1234, "Joe");
+		john.printInfo(1234);
+		john.setPassword(1234,"joeiscool");
+		john.printInfo(1234);
+		john.setBal(1234,6969);
+		john.printInfo(1234);
+		john.setPIN(1234, 4321);
+		john.printInfo(4321);
+		john.depositMoney(4321,1);
+		john.printInfo(4321);
+		john.withdrawMoney(4321,6970);
+		john.printInfo(4321);
+		
 	}
 } 
