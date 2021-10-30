58e0c1464e4c1f45dc87b1772d7a0fce78ba8cf1
Abdullah Faruque <90860472+afaruque30@users.noreply.github.com>
2021-10-30 09:23:17 -0400

    Update string2problems.java

--- cb/string-2/string2problems.java
+++ cb/string-2/string2problems.java
@@ -1,26 +1,5 @@
 public class string2problems {
 	
-public static void main(String[] args){
-	System.out.println("cases for helloName");
-	System.out.println(helloName("Hello Bob"));
-	System.out.println(helloName("Hello Joe"));
-	System.out.println(helloName("Hello Mama"));
-	System.out.println("\n");
-	System.out.println("cases for makeAbba");
-	System.out.println(makeAbba("Hello", "Mama"));
-	System.out.println(makeAbba("joe", "Mama"));
-	System.out.println(makeAbba("ok", "and"));
-	
-}
-
-public static String helloName(String name) {
-  return ("Hello " + name + "!");
-	}
-
-public static String makeAbba(String a, String b) {
-  		return (a+b+b+a);
-	}	
-}
     public static void main(String[] args) {
         System.out.println("cases for countHi");
         System.out.println(countHi("abc hi ho")); // 1
