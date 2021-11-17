468858be2e781964969f71df94b0d6b1191537f8
Abduller Maraco <afaruque@stuy.edu>
2021-11-16 21:18:02 -0500

    works now

--- cb/array-2/centeredAverage.java
+++ cb/array-2/centeredAverage.java
@@ -14,6 +14,16 @@ public class centeredAverage{
 
 
 public int centeredAverage(int[] nums) {
+  while (!ordered(nums)){
+  for(int i=0;i<nums.length-1;i++){
+    if ((nums[i]>=nums[i+1]) && (nums[i]!=nums[i+1])){
+      int temp = nums[i+1];
+      nums[i+1]=nums[i];
+      nums[i]=temp;
+    }
+  }
+  }
+ 
   if ((nums.length)%2==1){
     return nums[nums.length/2];
   }
@@ -22,9 +32,15 @@ public int centeredAverage(int[] nums) {
   }
 }
 
-
-public static void main(String[] args){
-	System.out.println(centeredAverage([1,2,6]);
-	System.out.println(centeredAverage([6,1,2]);
-	System.out.println(centeredAverage([1,2,3]);
+public boolean ordered(int [] nums){
+  for (int i=0;i<nums.length-1;i++){
+    if (nums[i]>nums[i+1]) return false;
+  }
+  return true;
 }
+
+public static void main(String[] args) {
+        System.out.println(centeredAverage(new int[] {1, 2, 3, 4, 100})); // 3
+        System.out.println(centeredAverage(new int[] {1, 1, 5, 5, 10, 8, 7})); // 5
+        System.out.println(centeredAverage(new int[] {-10, -4, -2, -4, -2, 0})); // -3
+    }
