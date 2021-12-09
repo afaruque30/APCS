fc5d9451e3849cd9dfec99dd7fa2ef0debb99824
Abduller Maraco <afaruque@stuy.edu>
2021-12-08 19:54:34 -0500

    added hw 45

---
+++ 45_suparr/ListInt.java
@@ -0,0 +1,22 @@
+/*
+ * Team Green: Abdullah Faruque, Neil Lin, Russell Goychayev
+ * APCS pd8
+ * HW45: Array of Titanium
+ * 2021-12-8
+ * time spent - 0.5 hours
+ *
+ * Disco- ...
+ *
+ * QCC- ...
+ *
+*/
+
+
+public interface ListInt {
+  public boolean add(int x);
+  public int get(int index );
+  public int set(int index, int newVal);
+  public int size();
+
+
+}
