f5881071b559c4925657c5135abf3ffe7218fdee
Abdullah Faruque <90860472+afaruque30@users.noreply.github.com>
2022-02-15 07:04:31 -0500

    Update bunnyEars2.java

--- cb/recursion-1/bunnyEars2.java
+++ cb/recursion-1/bunnyEars2.java
@@ -1,12 +1,32 @@
-public class bunnyEars2 {
-public int bunnyEars2(int bunnies) {
-  int ears = 0;
-  if ( bunnies == 0 )
-    return ears;
-  if ( bunnies % 2 == 1 )
-    ears = 2;
-  else 
-    ears = 3;
-  return ears + bunnyEars2( bunnies - 1 );
-}
-}
+ 
+ ​//​ Sweet Valentines : David Chen, Abdullah Faraque 
+ ​//​ APCS pd8 
+ ​//​ HW64: Revisitation 
+ ​//​ 2022-02-14m 
+ ​//​ time spent:  0.5 hrs 
+  
+ ​//​ We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, 
+ ​//​ ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 
+ ​//​ ears, because they each have a raised foot. Recursively return the number of 
+ ​//​ "ears" in the bunny line 1, 2, ... n (without loops or multiplication). 
+  
+  
+ ​public​ ​class​ ​bunnyEars2​ { 
+ ​    ​public​ ​static​ ​void​ ​main​(​String​[] ​args​) { 
+ ​        ​System​.​out​.​println(bunnyEars2(​0​)); 
+ ​        ​System​.​out​.​println(bunnyEars2(​1​)); 
+ ​        ​System​.​out​.​println(bunnyEars2(​2​)); 
+ ​    } 
+  
+ ​    ​public​ ​static​ ​int​ ​bunnyEars2​(​int​ ​bunnies​) { 
+ ​        ​if​ (bunnies ​==​ ​0​) { 
+ ​            ​return​ ​0​; 
+ ​        } 
+  
+ ​        ​if​ (bunnies ​%​ ​2​ ​==​ ​1​) { 
+ ​            ​return​ ​2​ ​+​ bunnyEars2(bunnies ​-​ ​1​); 
+ ​        } 
+  
+ ​        ​return​ ​3​ ​+​ bunnyEars2(bunnies ​-​ ​1​); 
+ ​    } 
+ ​}
