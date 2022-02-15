dbd536a296d4bd181ff64499a9fd25d5648ede0c
Abdullah Faruque <90860472+afaruque30@users.noreply.github.com>
2022-02-15 07:00:06 -0500

    Update fibonacci.java

--- cb/recursion-1/fibonacci.java
+++ cb/recursion-1/fibonacci.java
@@ -1,20 +1,35 @@
-public class fibonacci {
-    public static void main(String[] args) {
-        System.out.println(fibonacci(0));
-        System.out.println(fibonacci(1));
-        System.out.println(fibonacci(2));
-        System.out.println(fibonacci(3));
-
-    }
-
-    public static int fibonacci(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        }
-
-        return fibonacci(n - 1) + fibonacci(n-2);
-    }
-
-}
+ 
+ ​//​ Sweet Valentines : David Chen, Abdullah Faraque 
+ ​//​ APCS pd8 
+ ​//​ HW64: Revisitation 
+ ​//​ 2022-02-14m 
+ ​//​ time spent:  0.5 hrs 
+  
+ ​//​ The fibonacci sequence is a famous bit of mathematics, and it happens to have 
+ ​//​ a recursive definition. The first two values in the sequence are 0 and 1 
+ ​//​ (essentially 2 base cases). Each subsequent value is the sum of the previous 
+ ​//​ two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. 
+ ​//​ Define a recursive fibonacci(n) method that returns the nth fibonacci number, 
+ ​//​ with n=0 representing the start of the sequence. 
+  
+  
+ ​public​ ​class​ ​fibonacci​ { 
+ ​    ​public​ ​static​ ​void​ ​main​(​String​[] ​args​) { 
+ ​        ​System​.​out​.​println(fibonacci(​0​)); 
+ ​        ​System​.​out​.​println(fibonacci(​1​)); 
+ ​        ​System​.​out​.​println(fibonacci(​2​)); 
+ ​        ​System​.​out​.​println(fibonacci(​3​)); 
+  
+ ​    } 
+  
+ ​    ​public​ ​static​ ​int​ ​fibonacci​(​int​ ​n​) { 
+ ​        ​if​ (n ​==​ ​0​) { 
+ ​            ​return​ ​0​; 
+ ​        } ​else​ ​if​ (n ​==​ ​1​) { 
+ ​            ​return​ ​1​; 
+ ​        } 
+  
+ ​        ​return​ fibonacci(n ​-​ ​1​) ​+​ fibonacci(n​-​2​); 
+ ​    } 
+  
+ ​}
