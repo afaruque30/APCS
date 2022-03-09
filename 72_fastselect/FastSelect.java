ae11d67c67618cd7d4214983a212b229f16fd1b5
Abduller Maraco <afaruque@stuy.edu>
2022-03-08 21:45:01 -0500

    hw

--- 72_fastselect/FastSelect.java
+++ 72_fastselect/FastSelect.java
@@ -2,7 +2,23 @@
 // APCS pd8
 // HW72: So So Fast
 // 2022-03-08
-// time spent: 0.5 hrs
+// time spent: 1.1 hrs
+/*
+Algo:
+    Initialize lo and hi to 0 and n-1, respectively.
+    Let pvtIndex be the value returned by partition(arr, lo, hi, lo).
+    while pvtIndex != k, check if pvtIndex is less than k. if true, set lo to pvtIndex + 1 (increasing the lower bound)
+    otherwise, return arr[k - 1] because we know the first `pvtIndex` elements are sorted. Then partition (arr, lo, hi, lo). When pvtIndex = k, return arr[pvtIndex - 1].
+Best Case: 
+		The Best Case would be when the yth smallest value in the array is in the first position and its classification would be O(n). 
+Worst Case: 
+		The Worst Case would be when the yth smallest value in the array is in the last position and its classification would be 0(n^2). 
+DISCOS:
+		There is no need to change hi.
+QCC:
+		Is there a way to improve/optimize our code/algo? 
+*/
+
 
 public class FastSelect { 
     public static void swap( int x, int y, int[] o )
@@ -32,7 +48,7 @@ public class FastSelect {
       }
     }
       
-    //return int array of size s, with each element fr range [0,maxVal)
+    //return int array of size s, with each element fr range [0,maxVal]
     public static int[] buildArray( int s, int maxVal )
     {
       int[] retArr = new int[s];
@@ -84,7 +100,7 @@ public class FastSelect {
             if (pvtIndex < k) {
                 lo = pvtIndex + 1;
             } else {
-                hi = pvtIndex - 1;
+                return arr[k - 1];
             }
             pvtIndex = partition(arr, lo, hi, lo);
         }
@@ -97,12 +113,15 @@ public class FastSelect {
         int[] arr4 = {5,4,17,9000,6};
         int[] arr5 = {3,0,16,599,1024};
         int[] arr6 = {21, 17, 69, 343, 7, 23, 1223};
+        int[] bestCase = {21,7,17,69,343};
+        int[] worstCase = {7,17,69,343,21};
 
-        System.out.println(fastSelect(arr1, 3));
-        System.out.println(fastSelect(arr3, 3));
-        System.out.println(fastSelect(arr4, 3));
-        System.out.println(fastSelect(arr5, 3));
-        System.out.println(fastSelect(arr6, 3));
-
+        System.out.println(fastSelect(arr1, 3)); // 21
+        System.out.println(fastSelect(arr3, 3)); // 33
+        System.out.println(fastSelect(arr4, 3)); // 6
+        System.out.println(fastSelect(arr5, 3)); // 16
+        System.out.println(fastSelect(arr6, 3)); // 21
+        System.out.println(fastSelect(bestCase, 3)); // 21
+		    System.out.println(fastSelect(worstCase, 3)); // 21
     }
 }
