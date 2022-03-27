d1755880ca4d1861ca6ce7b4a37ded33a584a6a3
Abduller Maraco <afaruque@stuy.edu>
2022-03-26 22:24:08 -0400

    zed zed zed

--- 82_llist_itr/List.java
+++ 82_llist_itr/List.java
@@ -1,3 +1,4 @@
+
 /***
  * interface List
  * Specifies actions a List must be able to perform.
@@ -9,28 +10,27 @@
 
 import java.util.Iterator;
 
-public interface List<T> extends Iterable<T>
-{
-    //add element T to end of list
-    //always return true
-    public boolean add( T x );
+public interface List<T> extends Iterable<T> {
+    // add element T to end of list
+    // always return true
+    public boolean add(T x);
 
-    //insert element T at index i
-    public void add( int i, T newVal );
+    // insert element T at index i
+    public void add(int i, T newVal);
 
-    //remove element at index i
-    public T remove( int i );
+    // remove element at index i
+    public T remove(int i);
 
-    //return element at index i
-    public T get( int i );
+    // return element at index i
+    public T get(int i);
 
-    //overwrite element at index i, return old element at index i
-    public T set( int i, T x );
+    // overwrite element at index i, return old element at index i
+    public T set(int i, T x);
 
-    //return number of meaningful elements in list
+    // return number of meaningful elements in list
     public int size();
 
-    //return an Iterator over the elements in list
+    // return an Iterator over the elements in list
     Iterator<T> iterator();
 
-}//end interface List
+}// end interface List
--- 82_llist_itr/V6Driver.java
+++ 82_llist_itr/V6Driver.java
@@ -1,31 +1,49 @@
+import java.util.Iterator;
+
+// A Dog's Dream (David Chen + Daniel Jung + Abdullah Faraque)
+// APCS pd8
+// HW82: Roll Your Own Iterator
+// 2022-03-28m
+// time spent: 0.5 hrs
+
 /***
  * class V6Driver
  * ...for testing implementation of Iterable interface
- *    (ability to run a FOREACH loop over your LList)
+ * (ability to run a FOREACH loop over your LList)
  *
- * Assumes local version of List.java, overriding Java standard library interface
+ * Assumes local version of List.java, overriding Java standard library
+ * interface
  **/
 
-public class V6Driver
-{
-  public static void main( String[] args )
-  {
-    List<String> wu = new LList<String>();
+public class V6Driver {
+    public static void main(String[] args) {
+        List<String> wu = new LList<String>();
 
-    System.out.println(wu);
-    wu.add("RZA");
-    wu.add("GZA");
-    wu.add("ODB");
-    wu.add("Inspectah Deck");
-    wu.add("Raekwon the Chef");
-    wu.add("U-God");
-    wu.add("Ghostface");
-    wu.add("Method Man");
-    System.out.println(wu);
+        System.out.println(wu);
+        wu.add("RZA");
+        wu.add("GZA");
+        wu.add("ODB");
+        wu.add("Inspectah Deck");
+        wu.add("Raekwon the Chef");
+        wu.add("U-God");
+        wu.add("Ghostface");
+        wu.add("Method Man");
+        System.out.println(wu);
 
-    for( String s : wu ) {
-      System.out.println( s + " raps for the Wu");
-    }
-  }//end main()
+        for (String s : wu) {
+            System.out.println(s + " raps for the Wu");
+        }
 
-}//end class
+        System.out.println("Testing iterator remove");
+        System.out.println(wu);
+
+        Iterator<String> itr = wu.iterator();
+        while (itr.hasNext()) {
+            itr.next();
+            itr.remove();
+            System.out.println(wu);
+        }
+
+    }// end main()
+
+}// end class
